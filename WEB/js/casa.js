50755896460adc3d3dc642df2f725a6337cce97e
francircuito23 <fgo0002@alu.medac.es>
2022-05-17 13:16:55 +0200

    front

--- WEB/js/casa.js
+++ WEB/js/casa.js
@@ -1,28 +1,32 @@
-$(function () {
+window.onload = () =>{
 
-    $('.owl-carousel').owlCarousel({
-        
-        loop: true,
-        nav: true,
-        navText: ['<i class="fa-solid fa-caret-left"></i>', '<i class="fa-solid fa-caret-right"></i>'],
-        autoplay:true,
-        autoplayHoverPause: true,
-        dots: false,
-        responsive: {
-            0: {
-               items: 1, 
-               margin: 10,
-               dotsEach: 2
-            }, 
-            768:{
-                items:3,
-                margin: 10
-               
-            },
-            1500: {
-                items: 5,
-                margin: 1
-            }  
-        }
-    });
-})
+    $(function () {
+
+        $('.owl-carousel').owlCarousel({
+            
+            loop: true,
+            nav: true,
+            navText: ['<i class="fa-solid fa-caret-left"></i>', '<i class="fa-solid fa-caret-right"></i>'],
+            autoplay:true,
+            autoplayHoverPause: true,
+            dots: false,
+            responsive: {
+                0: {
+                   items: 1, 
+                   margin: 10,
+                   dotsEach: 2
+                }, 
+                768:{
+                    items:3,
+                    margin: 10
+                   
+                },
+                1500: {
+                    items: 5,
+                    margin: 1
+                }  
+            }
+        });
+    })
+}
+
